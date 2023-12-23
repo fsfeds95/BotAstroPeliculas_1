92ab8600cd33bc35e57ed064c5f3b7969d28be02
fsfeds95 <144530359+fsfeds95@users.noreply.github.com>
2023-12-22 23:42:52 -0400

    Actualizar bot.js

--- bot.js
+++ bot.js
@@ -1,111 +1,79 @@
-const { Telegraf } = require('telegraf');
+// Importar las dependencias necesarias
+const Telegraf = require('telegraf');
 const axios = require('axios');
 
-const token = '6980968831:AAG41Ii-Go9JMiIyXJKpjQHBFHj9AzDBXPI'; // Reemplaza con el token de tu bot de Telegram
-const apiKey = '74dc824830c7f93dc61b03e324070886'; // API key de TheMovieDB
+const bot = new Telegraf('6980968831:AAG41Ii-Go9JMiIyXJKpjQHBFHj9AzDBXPI');
 
-const bot = new Telegraf(token);
-
-bot.start((ctx) => ctx.reply('¡Hola! Envía el comando /buscar seguido del título de una película para buscar información de películas.'));
-
+// Comando /buscar para buscar películas por título en español o título original
 bot.command('buscar', async (ctx) => {
-  const query = ctx.message.text.split(' ').slice(1).join(' ');
+  const title = ctx.message.text.split(' ').slice(1).join(' ');
 
   try {
-    const response = await axios.get(
-      `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${encodeURIComponent(query)}`
-    );
+    const response = await axios.get('https://api.themoviedb.org/3/search/movie', {
+      params: {
+        api_key: '74dc824830c7f93dc61b03e324070886',
+        query: title,
+        language: 'es-MX'
+      }
+    });
 
-    if (response.data.results.length > 0) {
-      const movie = response.data.results[0];
+    const movie = response.data.results[0];
+    if (movie) {
       const posterPath = movie.poster_path;
-      const title = movie.title;
-      const originalTitle = movie.original_title;
+      const titleES = movie.title;
+      const titleOriginal = movie.original_title;
       const genres = movie.genres.map((genre) => genre.name).join(', ');
       const releaseYear = movie.release_date.split('-')[0];
       const originalLanguage = movie.original_language;
       const overview = movie.overview;
-      const trailer = await getMovieTrailer(movie.id);
 
-      const posterUrl = getPosterUrl(posterPath);
-      const trailerUrl = `https://www.youtube.com/watch?v=${trailer}`;
+      let imageUrl = `https://image.tmdb.org/t/p/original${posterPath}`;
+      imageUrl = imageUrl.replace('.jpg', '.svg') || imageUrl;
 
-      ctx.replyWithPhoto({ url: posterUrl }, { caption: `${title} (${originalTitle})` });
-      ctx.reply(`Géneros: ${genres}`);
-      ctx.reply(`Año de estreno: ${releaseYear}`);
-      ctx.reply(`Idioma original: ${originalLanguage}`);
-      ctx.reply(`Sinopsis: ${overview}`);
-      ctx.reply(`Trailer: ${trailerUrl}`);
+      await ctx.replyWithPhoto({ url: imageUrl }, { caption: `${titleES} (${titleOriginal})\nGéneros: ${genres}\nAño de estreno: ${releaseYear}\nIdioma original: ${originalLanguage}\nSinopsis: ${overview}` });
     } else {
-      ctx.reply('No se encontraron películas con ese título.');
+      await ctx.reply('No se encontraron resultados para la búsqueda.');
     }
   } catch (error) {
     console.error(error);
-    ctx.reply('Ocurrió un error al buscar la película.');
+    await ctx.reply('Ocurrió un error durante la búsqueda.');
   }
 });
 
+// Comando /id para buscar películas por ID
 bot.command('id', async (ctx) => {
-  const movieId = ctx.message.text.split(' ').slice(1).join(' ');
+  const movieId = ctx.message.text.split(' ')[1];
 
   try {
-    const response = await axios.get(
-      `https://api.themoviedb.org/3/movie/${movieId}?api_key=${apiKey}`
-    );
+    const response = await axios.get(`https://api.themoviedb.org/3/movie/${movieId}`, {
+      params: {
+        api_key: '74dc824830c7f93dc61b03e324070886',
+        language: 'es-MX'
+      }
+    });
 
     const movie = response.data;
-    const posterPath = movie.poster_path;
-    const title = movie.title;
-    const originalTitle = movie.original_title;
-    const genres = movie.genres.map((genre) => genre.name).join(', ');
-    const releaseYear = movie.release_date.split('-')[0];
-    const originalLanguage = movie.original_language;
-    const overview = movie.overview;
-    const trailer = await getMovieTrailer(movie.id);
+    if (movie) {
+      const posterPath = movie.poster_path;
+      const titleES = movie.title;
+      const titleOriginal = movie.original_title;
+      const genres = movie.genres.map((genre) => genre.name).join(', ');
+      const releaseYear = movie.release_date.split('-')[0];
+      const originalLanguage = movie.original_language;
+      const overview = movie.overview;
 
-    const posterUrl = getPosterUrl(posterPath);
-    const trailerUrl = `https://www.youtube.com/watch?v=${trailer}`;
+      let imageUrl = `https://image.tmdb.org/t/p/original${posterPath}`;
+      imageUrl = imageUrl.replace('.jpg', '.svg') || imageUrl;
 
-    ctx.replyWithPhoto({ url: posterUrl }, { caption: `${title} (${originalTitle})` });
-    ctx.reply(`Géneros: ${genres}`);
-    ctx.reply(`Año de estreno: ${releaseYear}`);
-    ctx.reply(`Idioma original: ${originalLanguage}`);
-    ctx.reply(`Sinopsis: ${overview}`);
-    ctx.reply(`Trailer: ${trailerUrl}`);
-  } catch (error) {
-    console.error(error);
-    ctx.reply('Ocurrió un error al obtener la película.');
-  }
-});
-
-async function getMovieTrailer(movieId) {
-  try {
-    const response = await axios.get(
-      `https://api.themoviedb.org/3/movie/${movieId}/videos?api_key=${apiKey}`
-    );
-
-    const videos = response.data.results.filter((video) => video.type === 'Trailer');
-
-    if (videos.length > 0) {
-      return videos[0].key;
+      await ctx.replyWithPhoto({ url: imageUrl }, { caption: `${titleES} (${titleOriginal})\nGéneros: ${genres}\nAño de estreno: ${releaseYear}\nIdioma original: ${originalLanguage}\nSinopsis: ${overview}` });
     } else {
-      return '';
+      await ctx.reply('No se encontraron resultados para la búsqueda.');
     }
   } catch (error) {
     console.error(error);
-    return '';
+    await ctx.reply('Ocurrió un error durante la búsqueda.');
   }
-}
+});
 
-function getPosterUrl(posterPath) {
-  if (posterPath) {
-    const baseUrl = 'https://image.tmdb.org/t/p/original';
-    const posterUrl = `${baseUrl}${posterPath}`;
-
-    return posterUrl.endsWith('.svg') ? posterUrl : `${posterUrl}.jpg`;
-  } else {
-    return '';
-  }
-}
-
+// Iniciar el bot
 bot.launch();
