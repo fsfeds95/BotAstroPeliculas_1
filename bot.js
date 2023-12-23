9344d395cd8c8b2bb4d2fdc11967b2d7d416c359
fsfeds95 <144530359+fsfeds95@users.noreply.github.com>
2023-12-23 00:35:01 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,72 +1,77 @@
-const Telegraf = require('telegraf');
+const express = require('express');
+const TelegramBot = require('node-telegram-bot-api');
 const axios = require('axios');
-const fs = require('fs');
-require('.env').config();
+const config = require('./config');
 
-const bot = new Telegraf(process.env.TELEGRAM_TOKEN);
+const bot = new TelegramBot(config.telegramToken, {polling: true});
+const app = express();
+const PORT = 8225;
 
-bot.start((ctx) => ctx.reply('¡Hola! Puedes buscar películas ingresando el título en español, el título original o el ID de la película.'));
+app.use(express.json());
 
-bot.command('buscar', async (ctx) => {
-  const title = ctx.message.text.split(' ').slice(1).join(' ');
-  const response = await axios.get(`https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIEDB_API_KEY}&query=${encodeURIComponent(title)}&language=es`);
+// Ruta para recibir los mensajes
+app.post(`/bot${config.telegramToken}`, (req, res) => {
+  bot.processUpdate(req.body);
+  res.sendStatus(200);
+});
 
-  if (response.data.results.length === 0) {
-    ctx.reply('No se encontraron resultados para esa búsqueda.');
-  } else {
+// Comando /buscar
+bot.onText(/\/buscar (.+)/, async (msg, match) => {
+  const chatId = msg.chat.id;
+  const query = match[1];
+
+  try {
+    const response = await axios.get(`https://api.themoviedb.org/3/search/movie`, {
+      params: {
+        api_key: config.movieDbKey,
+        query: query,
+        language: 'es'
+      }
+    });
+
     const movie = response.data.results[0];
-    const imageUrl = `https://image.tmdb.org/t/p/original${movie.poster_path}`;
-    const genres = movie.genres.map((genre) => genre.name).join(', ');
+    if (movie) {
+      const posterUrl = `https://image.tmdb.org/t/p/original${movie.poster_path}`;
+      const posterCaption = `Título: ${movie.title}\nTítulo original: ${movie.original_title}\nGénero: ${movie.genres.map(genre => genre.name).join(', ')}\nAño de estreno: ${movie.release_date.substring(0, 4)}\nIdioma original: ${movie.original_language}\nSinopsis: ${movie.overview}\nTrailer: https://www.youtube.com/watch?v=${movie.video_key}`;
 
-    const image = await axios.get(imageUrl, { responseType: 'arraybuffer' });
-    const resizedImage = await sharp(image.data).resize(1280, 720).toBuffer();
-
-    ctx.replyWithPhoto({ source: resizedImage });
-
-    ctx.reply(`Título en español: ${movie.title}`);
-    ctx.reply(`Título original: ${movie.original_title}`);
-    ctx.reply(`Géneros: ${genres}`);
-    ctx.reply(`Año de estreno: ${movie.release_date.slice(0, 4)}`);
-    ctx.reply(`Idioma original: ${movie.original_language}`);
-    ctx.reply(`Sinopsis: ${movie.overview}`);
-
-    const videoResponse = await axios.get(`https://api.themoviedb.org/3/movie/${movie.id}/videos?api_key=${process.env.MOVIEDB_API_KEY}`);
-    const trailerKey = videoResponse.data.results[0].key;
-    const trailerUrl = `https://www.youtube.com/watch?v=${trailerKey}`;
-
-    ctx.reply(`Trailer: ${trailerUrl}`);
+      bot.sendPhoto(chatId, posterUrl, { caption: posterCaption });
+    } else {
+      bot.sendMessage(chatId, 'No se encontró ninguna película con ese título');
+    }
+  } catch (error) {
+    console.error(error);
+    bot.sendMessage(chatId, 'Ocurrió un error al buscar la película');
   }
 });
 
-bot.command('id', async (ctx) => {
-  const movieId = ctx.message.text.split(' ')[1];
-  const response = await axios.get(`https://api.themoviedb.org/3/movie/${movieId}?api_key=${process.env.MOVIEDB_API_KEY}&language=es`);
+// Comando /id
+bot.onText(/\/id (.+)/, async (msg, match) => {
+  const chatId = msg.chat.id;
+  const movieId = match[1];
 
-  if (response.data) {
+  try {
+    const response = await axios.get(`https://api.themoviedb.org/3/movie/${movieId}`, {
+      params: {
+        api_key: config.movieDbKey,
+        language: 'es'
+      }
+    });
+
     const movie = response.data;
-    const imageUrl = `https://image.tmdb.org/t/p/original${movie.poster_path}`;
-    const genres = movie.genres.map((genre) => genre.name).join(', ');
+    if (movie) {
+      const posterUrl = `https://image.tmdb.org/t/p/original${movie.poster_path}`;
+      const posterCaption = `Título: ${movie.title}\nTítulo original: ${movie.original_title}\nGénero: ${movie.genres.map(genre => genre.name).join(', ')}\nAño de estreno: ${movie.release_date.substring(0, 4)}\nIdioma original: ${movie.original_language}\nSinopsis: ${movie.overview}\nTrailer: https://www.youtube.com/watch?v=${movie.video_key}`;
 
-    const image = await axios.get(imageUrl, { responseType: 'arraybuffer' });
-    const resizedImage = await sharp(image.data).resize(1280, 720).toBuffer();
-
-    ctx.replyWithPhoto({ source: resizedImage });
-
-    ctx.reply(`Título en español: ${movie.title}`);
-    ctx.reply(`Título original: ${movie.original_title}`);
-    ctx.reply(`Géneros: ${genres}`);
-    ctx.reply(`Año de estreno: ${movie.release_date.slice(0, 4)}`);
-    ctx.reply(`Idioma original: ${movie.original_language}`);
-    ctx.reply(`Sinopsis: ${movie.overview}`);
-
-    const videoResponse = await axios.get(`https://api.themoviedb.org/3/movie/${movie.id}/videos?api_key=${process.env.MOVIEDB_API_KEY}`);
-    const trailerKey = videoResponse.data.results[0].key;
-    const trailerUrl = `https://www.youtube.com/watch?v=${trailerKey}`;
-
-    ctx.reply(`Trailer: ${trailerUrl}`);
-  } else {
-    ctx.reply('No se encontró una película con ese ID.');
+      bot.sendPhoto(chatId, posterUrl, { caption: posterCaption });
+    } else {
+      bot.sendMessage(chatId, 'No se encontró ninguna película con ese ID');
+    }
+  } catch (error) {
+    console.error(error);
+    bot.sendMessage(chatId, 'Ocurrió un error al buscar la película');
   }
 });
 
-bot.launch();
+app.listen(PORT, () => {
+  console.log(`Bot listening on port ${PORT}`);
+});
